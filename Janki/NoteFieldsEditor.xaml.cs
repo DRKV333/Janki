413fa68afdddf11643d8fc71bd93cec70bebcb21
Kálmán Vince <30904009+DRKV333@users.noreply.github.com>
2021-05-05 21:43:26 +0200

    Moved note field editor to user control

---
+++ Janki/NoteFieldsEditor.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Windows.UI.Xaml.Controls;
+
+namespace Janki
+{
+    public sealed partial class NoteFieldsEditor : UserControl
+    {
+        public NoteFieldsEditor()
+        {
+            InitializeComponent();
+        }
+    }
+}
