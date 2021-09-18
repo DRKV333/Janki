174654d91ca7f691a7ddcf6ad711e30141e855e3
Kálmán Vince <30904009+DRKV333@users.noreply.github.com>
2021-09-18 16:07:28 +0200

    Added TreeView sample data

--- JankiAvalonia/SampleData.cs
+++ JankiAvalonia/SampleData.cs
@@ -2,15 +2,55 @@
 
 namespace JankiAvalonia
 {
+    public class TreeLevel1
+    {
+        public string? Text { get; set; }
+        public List<TreeLevel2>? Children { get; set; }
+    }
+
+    public class TreeLevel2
+    {
+        public string? Name { get; set; }
+    }
+
     public static class SampleData
     {
-        public static readonly List<string> ListOfStrings = new List<string>()
+        public static readonly IReadOnlyList<string> ListOfStrings = new List<string>()
         {
             "Item1",
             "Item2",
             "Item3",
             "Item4",
             "Item5"
+        
+        };
+
+        public static readonly IReadOnlyList<TreeLevel1> ListOfTreeItems = new List<TreeLevel1>()
+        {
+            new()
+            {
+                Text = "First",
+                Children = new()
+                {
+                    new() { Name = "Inner1" },
+                    new() { Name = "Inner2" },
+                    new() { Name = "Inner3" }
+                }
+            },
+            new()
+            {
+                Text = "No Inner",
+                Children = new() { }
+            },
+            new()
+            {
+                Text = "Second",
+                Children = new()
+                {
+                    new() { Name = "Inner1" },
+                    new() { Name = "Inner2" }
+                }
+            }
         };
     }
 }
