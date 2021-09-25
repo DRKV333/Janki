8ebc626fe1281c2eeb44aa3f5640e8df6a550103
Kálmán Vince <30904009+DRKV333@users.noreply.github.com>
2021-09-25 23:25:14 +0200

    Added data bindings for Card Type Editor

---
+++ JankiAvalonia/Behaviours/TextEditorBindingBehaviour.cs
@@ -0,0 +1,65 @@
+﻿using Avalonia;
+using Avalonia.Data;
+using Avalonia.Xaml.Interactivity;
+using AvaloniaEdit;
+using System;
+
+namespace JankiAvalonia.Behaviours
+{
+    public class TextEditorBindingBehaviour : Behavior<TextEditor>
+    {
+        public static readonly DirectProperty<TextEditorBindingBehaviour, string> TextProperty =
+            AvaloniaProperty.RegisterDirect<TextEditorBindingBehaviour, string>(nameof(Text),
+                x => x.Text,
+                (x, v) => x.Text = v,
+                "",
+                BindingMode.TwoWay);
+
+#pragma warning disable S3963 // "static" fields should be initialized inline
+        static TextEditorBindingBehaviour()
+        {
+            TextProperty.Changed.Subscribe(x =>
+            {
+                TextEditorBindingBehaviour behaviour = (TextEditorBindingBehaviour)x.Sender;
+                if (!behaviour.handling && behaviour.AssociatedObject != null)
+                {
+                    behaviour.handling = true;
+                    behaviour.AssociatedObject.Text = x.NewValue.Value;
+                    behaviour.handling = false;
+                }
+            });
+        }
+#pragma warning restore S3963 // "static" fields should be initialized inline
+
+        private bool handling = false;
+
+        private string text = "";
+        public string Text
+        {
+            get => text;
+            set => SetAndRaise(TextProperty, ref text, value);
+        }
+
+        protected override void OnAttached()
+        {
+            if (AssociatedObject != null)
+                AssociatedObject.TextChanged += AssociatedObject_TextChanged;
+        }
+
+        protected override void OnDetaching()
+        {
+            if (AssociatedObject != null)
+                AssociatedObject.TextChanged -= AssociatedObject_TextChanged;
+        }
+
+        private void AssociatedObject_TextChanged(object? sender, EventArgs e)
+        {
+            if (!handling && AssociatedObject != null)
+            {
+                handling = true;
+                Text = AssociatedObject.Text;
+                handling = false;
+            }
+        }
+    }
+}
---
+++ JankiAvalonia/Behaviours/TreeViewItemExpandOnClick.cs
@@ -0,0 +1,33 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using System;
+
+namespace JankiAvalonia.Behaviours
+{
+    public class TreeViewItemExpandOnClick : AvaloniaObject
+    {
+        public static readonly AttachedProperty<bool> ActiveProperty =
+            AvaloniaProperty.RegisterAttached<TreeViewItemExpandOnClick, TreeViewItem, bool>("Active");
+
+        public static bool GetActive(AvaloniaObject obj) => obj.GetValue(ActiveProperty);
+
+        public static void SetActive(AvaloniaObject obj, bool value) => obj.SetValue(ActiveProperty, value);
+
+        static TreeViewItemExpandOnClick()
+        {
+            ActiveProperty.Changed.Subscribe(x =>
+            {
+                if (x.NewValue.Value)
+                    ((TreeViewItem)x.Sender).PointerPressed += TreeViewItemExpandOnClick_PointerPressed;
+                else
+                    ((TreeViewItem)x.Sender).PointerPressed -= TreeViewItemExpandOnClick_PointerPressed;
+            });
+        }
+
+        private static void TreeViewItemExpandOnClick_PointerPressed(object? sender, Avalonia.Input.PointerPressedEventArgs e)
+        {
+            if (sender != null)
+                ((TreeViewItem)sender).IsExpanded = true;
+        }
+    }
+}
