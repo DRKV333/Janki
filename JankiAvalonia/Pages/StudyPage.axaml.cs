ed49c0456c14a789e8be2816f05d9dad6f6f8dc4
Kálmán Vince <30904009+DRKV333@users.noreply.github.com>
2021-09-26 20:04:54 +0200

    Added Dashboard page data bindings

--- JankiAvalonia/Pages/StudyPage.axaml.cs
+++ JankiAvalonia/Pages/StudyPage.axaml.cs
@@ -1,9 +1,11 @@
-using Avalonia;
 using Avalonia.Controls;
 using Avalonia.Markup.Xaml;
+using JankiAvalonia.Services;
+using JankiBusiness.ViewModels.Study;
 
 namespace JankiAvalonia.Pages
 {
+    [ViewForPage(typeof(StudyPageViewModel))]
     public partial class StudyPage : UserControl
     {
         public StudyPage()
